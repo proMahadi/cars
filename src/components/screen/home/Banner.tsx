20642963772be37726e37686f144298fa0e75196
proMahadi <professionalmahadi@gmail.com>
2024-12-28 18:53:07 +0600

    swiper data added and banner slider added

--- src/components/screen/home/Banner.tsx
+++ src/components/screen/home/Banner.tsx
@@ -1,14 +1,26 @@
-import BannerSlide from "@/components/common/BannerSlide"
+import BannerSlide from "@/components/common/BannerSlide";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
+import { bannerData} from "@/data/bannerData";
 
 
-const Banner:React.FC = () => {
+
+const Banner: React.FC = () => {
   return (
     <section>
-        <div className="container pt-[122px]">
-            <BannerSlide/>
-        </div>
+      <div className="container pt-[122px]">
+        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+          {bannerData.map((item) => (
+            <SwiperSlide>
+              <BannerSlide item={item}/>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
---
+++ src/data/bannerData.ts
@@ -0,0 +1,32 @@
+export interface BannerItem {
+    id:string,
+    brand:string,
+    model:string,
+    price:string,
+    img:string,
+}
+
+
+export const bannerData : BannerItem[]=[
+    {
+        id:"1",
+        brand:"porsche",
+        model:"911 gt3rs",
+        price:"700,000",
+        img:"[url(@/assets/bannerImg1.svg)]",
+    },
+    {
+        id:"2",
+        brand:"porsche",
+        model:"911 gt3rs",
+        price:"700,000",
+        img:"[url(@/assets/bannerImg1.svg)]",
+    },
+    {
+        id:"3",
+        brand:"porsche",
+        model:"911 gt3rs",
+        price:"700,000",
+        img:"[url(@/assets/bannerImg1.svg)]",
+    },
+]
